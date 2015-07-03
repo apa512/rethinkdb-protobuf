82ab630630e0fb1b0d50e1c1d5c7f7495efc3453
Daniel Compton <desk@danielcompton.net>
2015-07-03 12:29:21 +1200

    Upgrade lein-protobuf version

--- project.clj
+++ project.clj
@@ -5,4 +5,4 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.flatland/protobuf "0.8.1"]]
-  :plugins [[lein-protobuf "0.4.1"]])
+  :plugins [[lein-protobuf "0.4.3"]])
