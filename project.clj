d28c2e42fe204230f18185ecae83bd4c1d186608
Daniel Compton <desk@danielcompton.net>
2015-07-10 11:54:39 +1200

    Add release tasks

--- project.clj
+++ project.clj
@@ -5,4 +5,12 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.flatland/protobuf "0.8.1"]]
-  :plugins [[lein-protobuf "0.4.3"]])
+  :plugins [[lein-protobuf "0.4.3"]]
+  :release-tasks [["vcs" "assert-committed"]
+                  ["change" "version" "leiningen.release/bump-version" "release"]
+                  ["vcs" "commit"]
+                  ["vcs" "tag"]
+                  ["deploy" "clojars"]
+                  ["change" "version" "leiningen.release/bump-version"]
+                  ["vcs" "commit"]
+                  ["vcs" "push"]])
