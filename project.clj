c3dbfb84ac22a509d9fc85620a58363134f2e743
Daniel Compton <desk@danielcompton.net>
2015-07-10 11:35:27 +1200

    Move back to standard dependencies

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :url "http://github.com/apa512/clj-rethinkdb"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.6.0"]
-                                  [org.flatland/protobuf "0.8.1"]]}}
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.flatland/protobuf "0.8.1"]]
   :plugins [[lein-protobuf "0.4.3"]])
