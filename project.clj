37a707a83dd39255ef9e00245c2da1ecc9041d6e
Daniel Compton <desk@danielcompton.net>
2015-07-20 21:13:44 +1200

    Make dependency on protobuf :provided
    This means that the JAR we deploy doesn't need the protobuf dependency,
    and instead can just contain the generated classes. The protobuf
    dependency was never used at runtime before, but was bundled.

--- project.clj
+++ project.clj
@@ -1,10 +1,9 @@
-(defproject rethinkdb-protobuf "2.0.5"
+(defproject rethinkdb-protobuf "2.0.5-SNAPSHOT"
   :description "Protobufs for RethinkDB"
   :url "http://github.com/apa512/clj-rethinkdb"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.flatland/protobuf "0.8.1"]]
+  :profiles {:provided {:dependencies [[org.flatland/protobuf "0.8.1"]]}}
   :plugins [[lein-protobuf "0.4.3"]]
   :release-tasks [["vcs" "assert-committed"]
                   ["change" "version" "leiningen.release/bump-version" "release"]
