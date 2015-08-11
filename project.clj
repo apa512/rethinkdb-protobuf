21d195b6774ecef80bce3d438e291841f4acc778
Daniel Compton <desk@danielcompton.net>
2015-08-12 08:52:09 +1200

    Add :deploy-repositories, remove :release-tasks
    The standard release tasks are all that is necessary now we have
    :deploy-repositories specified. `lein deploy` will figure out whether to
    use releases or snapshots.

--- project.clj
+++ project.clj
@@ -2,14 +2,8 @@
   :description "Protobufs for RethinkDB"
   :url "http://github.com/apa512/clj-rethinkdb"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :profiles {:provided {:dependencies [[org.flatland/protobuf "0.8.1"]]}}
   :plugins [[lein-protobuf "0.4.3"]]
-  :release-tasks [["vcs" "assert-committed"]
-                  ["change" "version" "leiningen.release/bump-version" "release"]
-                  ["vcs" "commit"]
-                  ["vcs" "tag"]
-                  ["deploy" "clojars"]
-                  ["change" "version" "leiningen.release/bump-version"]
-                  ["vcs" "commit"]
-                  ["vcs" "push"]])
+  :deploy-repositories [["releases" :clojars]
+                        ["snapshots" :clojars]])
